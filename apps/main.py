81965cf3c13b8f81a6ba29192e28bbed685cc5c1
amyshindev <amysoo02@gmail.com>
2026-05-15 19:08:58 +0900

    .

--- apps/main.py
+++ apps/main.py
@@ -155,4 +155,4 @@ def read_doro_data():
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=, reload=True)
